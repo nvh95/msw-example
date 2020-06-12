a9b920eb9d6b1aeb6a9a34a673be624409ea21fb
Hung Viet Nguyen <henry@gotitapp.co>
2020-06-12 23:25:09 +0700

    add msw example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('old')
+  useEffect(()=> {
+    console.log('start fetching data')
+    fetch('/data')
+    .then(response=>response.json())
+    .then(result=>{
+      console.log({result})
+      setData(result.data)
+    })
+    .catch((error)=> {
+      console.log({error})
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* data: old/new */}
+      {`Data: ${data}`}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,64 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('UI should update after successfully calling a request', () => {
+  const server = setupServer(...[
+    rest.get('/data', (req, res, ctx) => res(ctx.json({
+      data: 'new',
+    }))),
+    rest.put('/data', (req, res, ctx) => res(ctx.json({})))
+  ]);
+
+  beforeAll(() => {
+    server.listen();
+  });
+  afterEach(() => server.resetHandlers());
+  afterAll(() => {
+    server.close();
+  });
+
+  // Not passed
+  fit('Logic after request is resolved DOES NOT execute', () => {
+    render(<App />);
+    // Response from server isn't logged to console
+    // screen.debug show UI before the request has done
+    screen.debug()
+    expect(screen.queryByText('Data: old')).not.toBeInTheDocument()
+    expect(screen.queryByText('Data: new')).toBeInTheDocument()
+  });
+
+  // Passed (5%)/ Not Passed (95%)
+  it('Logic after request is resolved DOES execute. (Use waitFor)', async () => {
+    const spy = jest.spyOn(global, 'fetch')
+    render(<App />);
+
+    // We wait for fetch to execute, not wait when fetch has done
+    // We expect to find a way to wait when fetch has done
+    await waitFor(() => expect(spy).toHaveBeenCalledTimes(1))
+    // Response from server is logged to console but UI hasn't updated yet
+    // Expect UI: `Data: new`
+    screen.debug()
+    expect(screen.queryByText('Data: old')).not.toBeInTheDocument()
+    expect(screen.queryByText('Data: new')).toBeInTheDocument()
+  });
+
+  // Passed
+  it('Logic after request is resolved DOES execute. (Use setTimeout)', (done) => {
+    render(<App />);
+
+    // It works but bad solution
+    setTimeout(() => {
+      //  UI updated
+      screen.debug()
+      // Expect to see the result logged to the console
+      // Expect UI: `Data: new`
+      // Response from server is logged to console but UI hasn't updated yet
+      expect(screen.queryByText('Data: old')).not.toBeInTheDocument()
+      expect(screen.queryByText('Data: new')).toBeInTheDocument()
+      done()
+    }, 1000)
+  });
 });
